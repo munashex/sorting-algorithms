0e0a8aa46b0daafd502e0a9b05bbe243ac88c5f9
munashe madhuna <79254789+munashex@users.noreply.github.com>
2022-05-13 11:50:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -76,8 +76,8 @@ function bubbleSort(arr) {
       if(arr.length <= 1) return arr 
   
       let middle = Math.floor(arr.length / 2) 
-      let leftSide = mergeSort(arr.splice(0, middle))
-      let rightSide = mergeSort(arr.splice(middle)) 
+      let leftSide = mergeSort(arr.slice(0, middle))
+      let rightSide = mergeSort(arr.slice(middle)) 
   
       return merge(leftSide, rightSide)
   }
