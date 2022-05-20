a2812f55fdc9e888a6e626541d3a7c7b272b207d
munashe madhuna <79254789+munashex@users.noreply.github.com>
2022-05-20 11:11:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -113,3 +113,38 @@ function quickSort(arr, start = 0, end = arr.length - 1) {
   return arr
 }
 console.log(quickSort([5,4, 3, 2, 1]))
+
+
+
+
+function getDigit(num, place) {
+  return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10
+}
+
+function digitCount(num) {
+  if (num === 0) return 1
+  return Math.floor(Math.log10(Math.abs(num))) + 1
+}
+
+function mostDigits(nums) {
+  let maxDigits = 0
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(nums[i]))
+  }
+  return maxDigits
+}
+
+function radixSort(arrOfNums) {
+  let maxDigitCount = mostDigits(arrOfNums)
+  for (let k = 0; k < maxDigitCount; k++) {
+    let digitBuckets = Array.from({ length: 10 }, () => []) // [[], [], [],...]
+    for (let i = 0; i < arrOfNums.length; i++) {
+      let digit = getDigit(arrOfNums[i], k)
+      digitBuckets[digit].push(arrOfNums[i])
+    }
+    // New order after each loop
+    arrOfNums = [].concat(...digitBuckets)
+  }
+  return arrOfNums
+}
+console.log(radixSort([1, 33, 444, 0, 3, 2]))
